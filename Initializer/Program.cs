e9410d379ec2dc747dde42ac837affcb7a88d531
kaz@innvtv.com <kaz@innvtv.com>
2018-05-30 14:28:35 -0700

    Started initializer

---
+++ Initializer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace initializer
+{
+    class Program
+    {
+        // AutoResetEvent to signal when to exit the application.
+        //private static readonly AutoResetEvent waitHandle = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing Registarion Microservices");
+            Console.WriteLine();
+            Console.WriteLine("Checking resources....");
+
+
+        }
+    }
+}
---
+++ Initializer/README.md
@@ -0,0 +1,2 @@
+# Initializer
+Console application that ensures all resources are in place, and creates them if they are not.
